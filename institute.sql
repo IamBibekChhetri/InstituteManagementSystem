0ab1be8e95a17e22c0d283be4dfaa43c1daa93b7
Vivek <chettribibek2020@gmail.com>
2022-12-02 16:35:34 +0545

    update code

--- institute.sql
+++ institute.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2022 at 08:25 AM
+-- Generation Time: Dec 02, 2022 at 11:49 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -40,7 +40,9 @@ CREATE TABLE `authors` (
 --
 
 INSERT INTO `authors` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Test', 'on', '2022-11-24 05:12:36', '2022-11-24 05:13:53');
+(1, 'Unity Hebert', 'on', '2022-12-01 05:02:21', '2022-12-01 05:02:21'),
+(2, 'Lillith Melendez', 'off', '2022-12-01 05:02:26', '2022-12-02 04:07:34'),
+(3, 'Aurelia Delaney', 'on', '2022-12-01 05:02:32', '2022-12-01 05:02:32');
 
 -- --------------------------------------------------------
 
@@ -63,7 +65,9 @@ CREATE TABLE `batches` (
 --
 
 INSERT INTO `batches` (`id`, `course_id`, `code`, `name`, `status`, `created_at`, `updated_at`) VALUES
-(2, 2, '256', 'CSit', 'on', '2022-11-24 05:02:28', '2022-11-24 05:02:28');
+(1, 1, '2', 'Quin Bray', 'on', '2022-12-01 05:01:25', '2022-12-01 05:01:25'),
+(2, 3, '36', 'Cathleen Cervantes', 'on', '2022-12-01 05:01:31', '2022-12-01 05:01:49'),
+(3, 2, '89', 'Rhoda Compton', 'on', '2022-12-01 05:01:39', '2022-12-01 05:01:39');
 
 -- --------------------------------------------------------
 
@@ -90,8 +94,35 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `batch_id`, `course_id`, `subject_id`, `author_id`, `name`, `details`, `published`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 1, 1, 'Digital Logic', 'hello', '2022-11-24', 'on', '2022-11-24 05:23:02', '2022-11-24 05:23:02'),
-(2, 2, 1, 1, 1, 'tesitinf', 'testing', '2022-11-25', 'on', '2022-11-25 05:02:39', '2022-11-25 05:02:39');
+(1, 1, 2, 1, 1, 'Rowan Cook', 'In vel exercitation', '1970-08-09', 'on', '2022-12-01 05:02:42', '2022-12-01 05:02:42'),
+(2, 3, 3, 1, 1, 'Basil Robinson', 'Suscipit veniam off', '1979-04-15', 'on', '2022-12-01 05:02:49', '2022-12-01 05:02:49'),
+(3, 3, 3, 2, 1, 'Uriel Pugh', 'Dolorem aliquam magn', '1987-08-12', 'on', '2022-12-01 05:02:56', '2022-12-01 05:02:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_infos`
+--
+
+CREATE TABLE `company_infos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `cname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `company_infos`
+--
+
+INSERT INTO `company_infos` (`id`, `cname`, `rname`, `address`, `phone`, `photo`, `pan`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'Nepal Creative Tech', 'Mr. Krishna Adhikari', 'Golpark-2', '98562312', '1668678879.png', '26-98-5623-78945', 'adhakarikrishna@gmail.com', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -105,7 +136,6 @@ CREATE TABLE `courses` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `details` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `duration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fees` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -115,9 +145,34 @@ CREATE TABLE `courses` (
 -- Dumping data for table `courses`
 --
 
-INSERT INTO `courses` (`id`, `code`, `name`, `details`, `duration`, `fees`, `status`, `created_at`, `updated_at`) VALUES
-(1, '326', 'test', 'test', '25', '256000', 'on', '2022-11-22 05:28:02', '2022-11-24 02:58:14'),
-(2, '236', 'Bachelor', 'bachelor in csit', '5', '50000', 'off', '2022-11-24 03:20:24', '2022-11-27 00:39:51');
+INSERT INTO `courses` (`id`, `code`, `name`, `details`, `duration`, `status`, `created_at`, `updated_at`) VALUES
+(1, '41', 'Hope Conley', 'Cupidatat delectus', '13', 'on', '2022-12-01 05:01:05', '2022-12-01 05:01:05'),
+(2, '43', 'Cameran Becker', 'Irure aute assumenda', '20', 'on', '2022-12-01 05:01:11', '2022-12-01 05:01:11'),
+(3, '29', 'Roth Spence', 'Sed atque facilis an', '24', 'on', '2022-12-01 05:01:18', '2022-12-01 05:01:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `enrollments`
+--
+
+INSERT INTO `enrollments` (`id`, `student_id`, `course_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, NULL, NULL),
+(2, 1, 1, '2022-12-02 03:12:14', '2022-12-02 03:12:14'),
+(3, 1, 3, '2022-12-02 03:12:55', '2022-12-02 03:12:55'),
+(4, 2, 2, '2022-12-02 03:13:11', '2022-12-02 03:13:11');
 
 -- --------------------------------------------------------
 
@@ -143,10 +198,8 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `fees` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
   `amount` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payment` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `transaction` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -156,8 +209,8 @@ CREATE TABLE `fees` (
 -- Dumping data for table `fees`
 --
 
-INSERT INTO `fees` (`id`, `student_id`, `amount`, `payment`, `transaction`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, '25000', 'Esewa', '2256as', 'on', '2022-11-24 05:29:11', '2022-11-24 05:29:11');
+INSERT INTO `fees` (`id`, `course_id`, `amount`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, '48', 'on', '2022-12-01 05:04:23', '2022-12-01 05:04:23');
 
 -- --------------------------------------------------------
 
@@ -167,9 +220,6 @@ INSERT INTO `fees` (`id`, `student_id`, `amount`, `payment`, `transaction`, `sta
 
 CREATE TABLE `instructors` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `batch_id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `age` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -186,8 +236,9 @@ CREATE TABLE `instructors` (
 -- Dumping data for table `instructors`
 --
 
-INSERT INTO `instructors` (`id`, `batch_id`, `course_id`, `subject_id`, `name`, `age`, `address`, `phone`, `photo`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 1, 'Rahul', '24', 'Charange', '98025625112', '1669288744.png', 'rahulbro@gmail.com', '123456789', 'on', '2022-11-24 05:34:04', '2022-11-24 05:34:04');
+INSERT INTO `instructors` (`id`, `name`, `age`, `address`, `phone`, `photo`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Jael Greene', '25', 'Justin Rosario', '98562232155', '1669891692.png', 'dejafulere@mailinator.com', 'Pa$$w0rd!', 'on', '2022-12-01 05:03:12', '2022-12-02 01:36:56'),
+(2, 'Dominic Pierce', '96', 'Alec Bullock', '9865654221212', '1669965982-png', 'cujeqy@mailinator.com', 'Pa$$w0rd!', 'on', '2022-12-01 05:03:27', '2022-12-02 01:41:22');
 
 -- --------------------------------------------------------
 
@@ -220,7 +271,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (12, '2022_11_15_054016_create_payments_table', 1),
 (13, '2022_11_15_054040_create_students_table', 1),
 (14, '2022_11_15_054059_create_subjects_table', 1),
-(15, '2022_11_21_054716_create_user_roles_table', 1);
+(15, '2022_11_21_054716_create_user_roles_table', 1),
+(16, '2022_11_29_070912_create_company_infos_table', 1),
+(17, '2022_11_29_095017_create_enrollments_table', 2);
 
 -- --------------------------------------------------------
 
@@ -231,19 +284,15 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 CREATE TABLE `noticeboards` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `attachement` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `end` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `priority` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `noticeboards`
---
-
-INSERT INTO `noticeboards` (`id`, `title`, `attachement`, `priority`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Admission Open', '1668597752.png', '2', 'on', '2022-11-24 05:37:58', '2022-11-24 05:37:58');
 
 -- --------------------------------------------------------
 
@@ -268,6 +317,7 @@ CREATE TABLE `payments` (
   `student_id` int(11) NOT NULL,
   `payed` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `payment` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `transaction` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -277,8 +327,9 @@ CREATE TABLE `payments` (
 -- Dumping data for table `payments`
 --
 
-INSERT INTO `payments` (`id`, `student_id`, `payed`, `payment`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, '2562012', 'Esewa', 'on', '2022-11-24 05:45:03', '2022-11-24 05:45:03');
+INSERT INTO `payments` (`id`, `student_id`, `payed`, `payment`, `transaction`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, '100', 'Prabhu Pay', 'In sit omnis sunt ni', 'on', '2022-12-01 05:16:45', '2022-12-01 05:16:58'),
+(2, 2, '68', 'QPay Nepal', 'Suscipit doloremque', 'on', '2022-12-01 05:16:51', '2022-12-01 05:16:51');
 
 -- --------------------------------------------------------
 
@@ -307,8 +358,6 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `students` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `batch_id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `DOB` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -333,8 +382,9 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `batch_id`, `course_id`, `name`, `gender`, `DOB`, `father`, `mother`, `address`, `state`, `city`, `zipcode`, `nationality`, `phone`, `qualification`, `photo`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 'Rahul', 'Male', '2022-11-24', 'test', 'testing', 'butwal', 'state 5', 'butwal', '32659', 'nepal', '988633323', 'High level', '1669288432.png', 'Vivkr@gmail.com', '123456789', 'on', '2022-11-24 05:28:52', '2022-11-24 05:28:52');
+INSERT INTO `students` (`id`, `name`, `gender`, `DOB`, `father`, `mother`, `address`, `state`, `city`, `zipcode`, `nationality`, `phone`, `qualification`, `photo`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Wesley Cooper', 'Male', '2022-07-25', 'Cara Flores', 'Hasad Dodson', 'Rebecca Hayes', 'Allen Sutton', 'Kaitlin Ellis', 'Norman Osborne', 'Simone Morales', '14', 'Whilemina Graves', '1669891735.png', 'petop@mailinator.com', 'Pa$$w0rd!', 'on', '2022-12-01 05:03:55', '2022-12-01 05:03:55'),
+(2, 'Shad Wilkinson', 'Other', '1984-02-10', 'Bryar Vazquez', 'Stella Sims', 'Bree Joyner', 'Pearl Grimes', 'Lunea Britt', 'Juliet Guzman', 'Graham Huffman', '68', 'Martha Wade', '1669891751.png', 'pilut@mailinator.com', 'Pa$$w0rd!', 'on', '2022-12-01 05:04:11', '2022-12-01 05:04:11');
 
 -- --------------------------------------------------------
 
@@ -357,7 +407,8 @@ CREATE TABLE `subjects` (
 --
 
 INSERT INTO `subjects` (`id`, `batch_id`, `course_id`, `name`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 'Digital Logic', 'on', '2022-11-24 05:22:30', '2022-11-24 05:22:30');
+(1, 2, 3, 'Lee Gilmore', 'on', '2022-12-01 05:02:01', '2022-12-01 05:02:01'),
+(2, 2, 1, 'Indira Stanley', 'on', '2022-12-01 05:02:08', '2022-12-01 05:02:08');
 
 -- --------------------------------------------------------
 
@@ -385,8 +436,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `photo`, `user_role_id`, `name`, `address`, `phone`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(2, '1668597752.png', 1, 'Vivek admin', 'butwal', '9865646562', 'admin@gmail.com', NULL, '$2y$10$QeJmI.MvJ/zS.OGpRphC1eos7WCEuxFJ5Sw5MI2mJM2HQf7qwefVC', NULL, '2022-11-21 04:46:04', '2022-11-27 00:48:16'),
-(3, '1669101373.png', 1, 'Rahul', 'Charange', '985656223', 'rahul@gmail.com', NULL, '123456789', NULL, '2022-11-21 05:17:18', '2022-11-22 01:31:13');
+(1, '1669891422.png', 1, 'Vivek', 'Butwal', '98566523', 'admin@gmail.com', NULL, '123456789', NULL, NULL, '2022-12-01 04:58:42'),
+(3, '1669969036.png', 1, 'Vivek', 'Butawl', '9856621', 'user@gmail.com', NULL, '$2y$10$fDOvZnb/bNw.o8BzKV2Th.wLU98bVCKj4yXlb1aQCOBgm6zDIskZq', NULL, '2022-12-01 23:26:11', '2022-12-02 02:32:16');
 
 -- --------------------------------------------------------
 
@@ -407,8 +458,7 @@ CREATE TABLE `user_roles` (
 --
 
 INSERT INTO `user_roles` (`id`, `role`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'on', '2022-11-21 05:16:23', '2022-11-21 05:16:23'),
-(2, 'Admin', 'off', '2022-11-23 00:42:52', '2022-11-25 02:04:24');
+(1, 'Admin', 'on', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -433,9 +483,21 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `company_infos`
+--
+ALTER TABLE `company_infos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `courses`
 --
 ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -522,25 +584,37 @@ ALTER TABLE `user_roles`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `batches`
 --
 ALTER TABLE `batches`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `company_infos`
+--
+ALTER TABLE `company_infos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -558,25 +632,25 @@ ALTER TABLE `fees`
 -- AUTO_INCREMENT for table `instructors`
 --
 ALTER TABLE `instructors`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `noticeboards`
 --
 ALTER TABLE `noticeboards`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -588,25 +662,25 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_roles`
 --
 ALTER TABLE `user_roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
