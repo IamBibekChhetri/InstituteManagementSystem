5d99e7d6ea3cabe870eb4690a301c567c49d3e26
Vivek <chettribibek2020@gmail.com>
2022-12-23 11:57:00 +0545

    avater photo

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -4,7 +4,17 @@
 <!-- .page-cover -->
 <header class="page-cover">
               <div class="text-center">
-                <div class="user-avatar user-avatar-xl"><img src="{{asset('public/image/user/'.auth()->user()->photo)}}" alt=""></div>
+                <div class="user-avatar user-avatar-xl">
+                @if(auth()->user()->photo !== "")
+                  @if (file_exists('public/image/user/'.auth()->user()->photo))
+                  <img src="{{asset('public/image/user/'.auth()->user()->photo)}}" alt=""> 
+                  @else
+                  <img src="https://ui-avatars.com/api/?name={{ auth()->user()->name }}" alt="">
+                  @endif
+                  @else
+                  <img src="https://ui-avatars.com/api/?name={{ auth()->user()->name }}" alt="">
+                  @endif
+                </div>
                 <h2 class="h4 mt-2 mb-0"> {{auth()->user()->name}}</h2><hr>
                 
                 <p class="text-muted"> {{auth()->user()->user_role->role}}</p>
